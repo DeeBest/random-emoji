f276e8dcb9b635d4110f4316cd77f1486b0e70aa
DeeBest <simphiwedladla8@gmail.com>
2024-05-03 13:52:08 +0200

    added the emoji name and button to javascript

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const emojiBtn = document.getElementById('emoji-button');
+const emojiName = document.getElementById('emoji-name');
